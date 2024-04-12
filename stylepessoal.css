8cc3f92cdcce763bb0f786687fd48bd0b013edd5
tfsLua <165962622+tfsLua@users.noreply.github.com>
2024-04-12 17:27:33 -0300

    Add files via upload

---
+++ stylepessoal.css
@@ -0,0 +1,73 @@
+{
+    margin: 0.3%;
+    box-sizing: border-box;
+    font-family: Arial;
+    text-decoration: none;
+    padding: 0.3%;
+}
+header{
+    height: 120px;
+}
+
+font-size: 50px;
+    padding-left: 20px;
+    margin-top: 9%;
+    letter-spacing: 2px;
+    color: blueviolet;
+}
+.container .clique{
+    width: 160px;
+    height: 40px;
+    background:blueviolet;
+    border: none;
+    margin-bottom: 10px;
+    font-size: 18px;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: .4s ease;
+}
+.container .clique a{
+    text-decoration: none;
+    color: black;
+    transition: .3s ease;
+}
+.clique:hover{
+    background-color:rgb(55, 143, 170);
+}
+.container .titulo{
+    color:rgb(115, 225, 245);
+    font-size: 60px;
+    font-family: Arial, Helvetica, sans-serif;
+}
+body{
+    background: url(foto\ de\ fundo.jpg) no-repeat;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    position: relative;
+    background-size: cover;
+    background-position:  center center;
+}
+span{
+    color: rgb(82, 187, 219);
+    font-size: 20px;
+}
+p{
+font-family: Arial, Helvetica, sans-serif;
+font-size: 16px;
+}
+h1{
+    font-family:Arial, Helvetica, sans-serif ;
+    font-size: 20px;
+}
+h2{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+}
+img{
+    width: 150px;
+    height: 150px;
+}
+a{
+    text-decoration: none;
+}
